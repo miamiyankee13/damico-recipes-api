3ed6dd9bb87817c80456503dd9d7b3930e34ea44
Anthony Damico <anthony.damico721@gmail.com>
2019-04-12 17:09:48 -0400

    updated models.js

---
+++ models.js
@@ -0,0 +1,36 @@
+'use strict'
+//import dependencies
+const mongoose = require('mongoose');
+
+//configure mongoose to use ES6 promises & createIndex
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+
+//create recipe schema
+const recipeSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    ingredients: [{ type: String, required: true }],
+    instructions: [{ type: String, required: true }],
+    sides: [{ type: String, required: true }],
+    meal: { type: String, required: true },
+    type: { type: String, required: true }
+});
+
+//create method to control data sent to client
+recipeSchema.methods.serialize = function() {
+    return {
+        _id: this.id,
+        name: this.name,
+        ingredients: this.ingredients,
+        instructions: this.instructions,
+        sides: this.sides,
+        meal: this.meal,
+        type: this.type
+    }
+}
+
+//create mongoose model
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+//export mongoose model
+module.exports = { Recipe };
