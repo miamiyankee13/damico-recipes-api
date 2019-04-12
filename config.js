f3adc535784da9e37ae4c1af75ace0e2fcbf84e1
Anthony Damico <anthony.damico721@gmail.com>
2019-04-12 15:51:29 -0400

    added client origin

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/recipe-app";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/recipe-app";
 exports.PORT = process.env.PORT || 8080;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
