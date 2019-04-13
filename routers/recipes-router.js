41c0b4f53ce94d61e5b65f8e20d1fb2a9fea8042
Anthony Damico <anthony.damico721@gmail.com>
2019-04-13 10:18:00 -0400

    added POST route

--- routers/recipes-router.js
+++ routers/recipes-router.js
@@ -25,5 +25,46 @@ router.get('/', (req, res) => {
             });
 });
 
+//POST - create recipe
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['name', 'ingredients', 'instructions', 'sides', 'meal', 'type'];
+    const missingField = requiredFields.find(field => !(field in req.body));
+
+    if (missingField) {
+        const message = `Missing ${missingField} in request body`;
+        console.error(message);
+        return res.status(400).json({ message });
+    }
+
+    return Recipe.findOne({ name: req.body.name })
+            .then(recipe => {
+                if (recipe) {
+                    const message = 'Recipe already exists';
+                    console.error(message);
+                    return res.status(400).json({ message });
+                } else {
+                    return Recipe.create({
+                        name: req.body.name,
+                        ingredients: req.body.ingredients,
+                        instructions: req.body.instructions,
+                        sides: req.body.sides,
+                        meal: req.body.meal,
+                        type: req.body.type
+                    })
+                    .then(recipe => {
+                        return res.status(201).json(recipe.serialize());
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        return res.status(400).json({ message: 'Bad request' });
+                    });
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                return res.status(500).json({ message: 'Something went wrong'});
+            });
+});
+
 //export router instance
 module.exports = router;
