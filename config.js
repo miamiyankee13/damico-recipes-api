096932a1554a0d35ef85aced7377e303f04089b7
Anthony Damico <anthony.damico721@gmail.com>
2019-04-16 10:05:15 -0400

    updated test database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict'
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/recipe-app";
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/recipe-app";
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-recipe-app";
 exports.PORT = process.env.PORT || 8080;
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
